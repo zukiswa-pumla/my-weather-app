cebe550b8071729b70e8719b6f47f0ba35ac00b8
zukiswa-pumla <145932722+zukiswa-pumla@users.noreply.github.com>
2024-01-14 21:12:08 +0200

    Injected more weather data

--- src/index.js
+++ src/index.js
@@ -1,19 +1,46 @@
-function refreshWeather(response){
-    let tempElement = document.querySelector("#temp");
-    let temp = response.data.temperature.current;
-    let cityElement = document.querySelector("#city");
+function refreshWeather(response) {
+  let tempElement = document.querySelector("#temp");
+  let temp = response.data.temperature.current;
+  let cityElement = document.querySelector("#city");
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windSpeedElement = document.querySelector("#wind-speed");
+  let timeElement = document.querySelector("#time");
+  let date = new Date(response.data.time * 1000);
 
-    cityElement.innerHTML = response.data.city;
-    tempElement.innerHTML = Math.round(temp) ;
-
+  cityElement.innerHTML = response.data.city;
+  timeElement.innerHTML = formateDate(date);
+  descriptionElement.innerHTML = response.data.condition.description;
+  humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
+  windSpeedElement.innerHTML = `${response.data.wind.speed}km/h`;
+  tempElement.innerHTML = Math.round(temp);
 }
 
-function searchCity(city){
-    //make api call and update the interface
-    let apiKey = "2c8a60b8355fa86dfba81o42d846tee8";
-    let apiUrl =
-      `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
-    axios.get(apiUrl).then(refreshWeather);
+function formateDate(date) {
+  let minutes = date.getMinutes();
+  let hours = date.getHours();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  return `${day} ${hours}:${minutes}`;
+}
+function searchCity(city) {
+  //make api call and update the interface
+  let apiKey = "2c8a60b8355fa86dfba81o42d846tee8";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(refreshWeather);
 }
 
 function handleSearchSubmit(event) {
